5825cfa0b87cdfb37d15466ee0e6c7218024e0f6
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2025-01-08 18:35:12 +0100

    floating kabab

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { UI } from "./components/UI";
+import { Suspense } from "react";
+import { Experience } from "./components/Experience";
 
 function App() {
   return (
@@ -10,7 +12,11 @@ function App() {
       <Canvas shadows camera={{ position: [-2, 1, 5], fov: 30 }}>
         <color attach="background" args={["#501899"]} />
         <fog attach="fog" args={["#501899", 5, 20]} />
-       
+        <group position-y={-1}>
+          <Suspense fallback={null}>
+            <Experience />
+          </Suspense>
+        </group>
       </Canvas>
     </>
   );
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Environment, Float, Gltf, TransformControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useAtom } from "jotai";
+import { useRef } from "react";
+import { MathUtils } from "three";
+import { skibidiAtom } from "./UI";
+export const Experience = () => {
 
-const Experience = () => {
+
   return (
-    <div>Experience</div>
-  )
-}
-
-export default Experience
+    <>
+      <Environment preset="sunset" />
+        <Float
+          position={[2.5, 1, 1]}
+          speed={6}
+          floatIntensity={3}
+          rotationIntensity={2}
+        >
+          <Gltf scale={0.2} src="/models/Kebab.glb" castShadow />
+        </Float>
+        <Float
+          position={[-2.5, 1, 1]}
+          speed={8}
+          floatIntensity={2}
+          rotationIntensity={6}
+        >
+          <Gltf scale={0.2} src="/models/Kebab.glb" castShadow />
+        </Float>
+        <Float
+          position={[0, 1, -2.5]}
+          speed={12}
+          rotationIntensity={4}
+          floatIntensity={3}
+        >
+          <Gltf scale={0.2} src="/models/Kebab.glb" castShadow />
+        </Float>
+     
+    </>
+  );
+};
