05f10b5b196def0e6007984605466ad3967a8e56
hamza.rahmoun <hamza.rahmoun456@gmail.com>
2025-01-08 18:27:57 +0100

    start and stop bottom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-
-import './App.css'
+import { Loader } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { UI } from "./components/UI";
 
 function App() {
-
   return (
-  <div>
-    Hamza
-  </div>
-  )
+    <>
+      <UI/>
+      <Loader />
+      <Canvas shadows camera={{ position: [-2, 1, 5], fov: 30 }}>
+        <color attach="background" args={["#501899"]} />
+        <fog attach="fog" args={["#501899", 5, 20]} />
+       
+      </Canvas>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Skibidi.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Skibidi = () => {
+  return (
+    <div>Skibidi</div>
+  )
+}
+
+export default Skibidi
